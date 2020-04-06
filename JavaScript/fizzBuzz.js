de37f4e5c0db358b1c7f51b4f49c2362534426bb
johnkirtley <kirtleyj16@gmail.com>
2020-04-05 17:38:33 -0700

    New challenge

---
+++ JavaScript/fizzBuzz.js
@@ -0,0 +1,15 @@
+for (let i = 1; i <= 100; i++) {
+	let output = '';
+
+	if (i % 15 === 0) {
+		output += 'Fizz Buzz';
+	} else if (i % 3 === 0) {
+		output += 'Fizz ';
+	} else if (i % 5 === 0) {
+		output += 'Buzz';
+	} else {
+		output += i;
+	}
+
+	console.log(output);
+}
