9e6fa2df0d40c619eca0cd60db32e13bb6d94a7a
johnkirtley <kirtleyj16@gmail.com>
2020-04-13 17:24:18 -0700

    Counting elements problem

---
+++ JavaScript/countingElements.js
@@ -0,0 +1,20 @@
+const countingElements = (arr) => {
+	let count = 0;
+
+	for (let i = 0; i < arr.length; i++) {
+		for (let j = 0; j < arr.length; j++) {
+			if (arr[j] === arr[i] + 1) {
+				count++;
+				break;
+			}
+		}
+	}
+	return count;
+};
+
+module.exports = countingElements;
+
+// // test
+
+let array = [1, 1, 3, 3, 5, 5, 7, 7];
+console.log(countingElements(array));
