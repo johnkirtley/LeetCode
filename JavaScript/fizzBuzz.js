0cabeba34306ee90b8d775594abdf6e3588fd9c5
johnkirtley <kirtleyj16@gmail.com>
2020-05-28 17:04:20 -0700

    Updated Challenges

--- JavaScript/fizzBuzz.js
+++ JavaScript/fizzBuzz.js
@@ -4,7 +4,7 @@ for (let i = 1; i <= 100; i++) {
 	if (i % 15 === 0) {
 		output += 'Fizz Buzz';
 	} else if (i % 3 === 0) {
-		output += 'Fizz ';
+		output += 'Fizz';
 	} else if (i % 5 === 0) {
 		output += 'Buzz';
 	} else {
--- JavaScript/mergeSortedArrays.js
+++ JavaScript/mergeSortedArrays.js
@@ -1,13 +1,12 @@
 const mergeArrays = (arr1, arr2) => {
-    // Concatenate arrays
-    const merged = arr1.concat(arr2)
+	// Concatenate arrays
+	const merged = arr1.concat(arr2);
 
-    // Sort arrays
-    return merged.sort((a, b) => a - b)
-}
-
+	// Sort arrays
+	return merged.sort((a, b) => a - b);
+};
 
 // Test // Takes O(n) time
-const test1 = [1, 2, 3, 45, 5, 6]
-const test2 = [5, 7, 7, 7, 4, 2]
-console.log(mergeArrays(test1, test2))
+const test1 = [1, 2, 3, 45, 5, 6];
+const test2 = [5, 7, 7, 7, 4, 2];
+console.log(mergeArrays(test1, test2));
