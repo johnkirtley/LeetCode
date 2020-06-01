bd7ae9701823e436a324bfc8a365c729abd19ebf
johnkirtley <kirtleyj16@gmail.com>
2020-06-01 16:27:20 -0700

    Updated Challenge

--- JavaScript/removeDupes.js
+++ JavaScript/removeDupes.js
@@ -2,7 +2,8 @@ const removeDuplicates = (arr) => {
 	let hash = {};
 
 	for (let i = 0; i < arr.length; i++) {
-		let current = arr[i];
+		let current = [arr[i]];
+
 		if (!hash[current]) {
 			hash[current] = arr[i];
 		}
